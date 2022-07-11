eea6f1c3b9cce9c63edcc84ff75022263453c777
Cymelo92 <58783892+Cymelo92@users.noreply.github.com>
2022-07-11 11:00:42 +0200

    changes
    changed the README file and the setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="TemplatePythonPackage",
+    name="CarloTestPackage",
     version="0.1.0",
     description="A Minimal Template Python Package",
-    url="https://github.com/RealityBending/TemplatePythonPackage",
-    author="Dominique Makowski",
-    author_email="dom.makowski@gmail.com",
+    url="https://github.com/Cymelo92/CarloTestPackage",
+    author="Carlo Meloni",
+    author_email="something.something@gmail.com",
     license="MIT",
     install_requires=["numpy", "pandas", "scipy", "matplotlib"],
     packages=find_packages(),
